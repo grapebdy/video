5274d44ae65ed74115aa0ae303c4b313a2f96a7b
grapebdy <grapel2013@gmail.com>
2018-03-15 09:41:40 +0800

    fb: Add draw function

--- fb/test-fb.c
+++ fb/test-fb.c
@@ -7,6 +7,89 @@
 
 #define GPT_FB_DEV	"/dev/fb0"
 
+int gpt_fb_draw_copy(struct fb_var_screeninfo *vinfo, struct fb_fix_screeninfo *finfo, unsigned char *fb_base,
+			unsigned char *buff, unsigned int xres, unsigned int yres)
+{
+	int row, column;
+	int pixel = 0;
+	unsigned int location;
+
+	printf("xres: %d, yres: %d\n", xres, yres);
+
+	for (row = 0; row  < yres; row++)
+		for (column = 0; column < xres; column++, pixel++) {
+			location = (vinfo->xoffset + column) * (vinfo->bits_per_pixel / 8) +
+				(vinfo->yoffset + row) * finfo->line_length;
+			if (vinfo->bits_per_pixel == 32) {
+				*(fb_base + location + 0) = *(buff + pixel * 4 + 0);
+				*(fb_base + location + 1) = *(buff + pixel * 4 + 1);
+				*(fb_base + location + 2) = *(buff + pixel * 4 + 2);
+				*(fb_base + location + 3) = *(buff + pixel * 4 + 3);
+			} else {
+				*(fb_base + location + 0) = *(buff + pixel * 2 + 0);
+				*(fb_base + location + 1) = *(buff + pixel * 2 + 1);
+			}
+		}
+
+}
+unsigned char buff[0x100000];
+
+int generate_data(int type, unsigned char *buf)
+{
+	int i = 0;
+
+	int r;
+	int g;
+	int b;
+
+	switch (type) {
+		for (i = 0; i < 0x4000; i+=4) {
+			buf[i + 0] = 10;
+			buf[i + 1] = 100;
+			buf[i + 2] = 100;
+			buf[i + 3] = 0;
+		}
+		break;
+	case 1:
+		r = 0xff & 0x1f;
+		g = 0x00  & 0x3f;
+		b = 0x00 & 0x1f;
+		for (i = 0; i < 0x80000; i++) {
+			((unsigned short *)buf)[i] = r << 11 | g << 5 | b;
+		}
+		break;
+
+	case 2:
+		r = 0x00 & 0x1f;
+		g = 0xff  & 0x3f;
+		b = 0x00 & 0x1f;
+		for (i = 0; i < 0x80000; i++) {
+			((unsigned short *)buf)[i] = r << 11 | g << 5 | b;
+		}
+		break;
+
+	case 3:
+		r = 0x00 & 0x1f;
+		g = 0x00  & 0x3f;
+		b = 0xff & 0x1f;
+		for (i = 0; i < 0x80000; i++) {
+			((unsigned short *)buf)[i] = r << 11 | g << 5 | b;
+		}
+		break;
+
+	case 4:
+		r = 0x00 & 0x1f;
+		g = 0xff  & 0x3f;
+		b = 0xff & 0x1f;
+		for (i = 0; i < 0x80000; i++) {
+			((unsigned short *)buf)[i] = r << 11 | g << 5 | b;
+		}
+		break;
+
+	}
+	return 0;
+}
+
 int main()
 {
 	int fbfd = -1;
@@ -40,7 +123,50 @@ int main()
 		close(fbfd);	
 		return -1;
 	}
-	memset(fb_base, 0xfe51fe5a, screen_size);
+
+	generate_data(1, buff);
+	vinfo.xoffset = 0;
+	vinfo.yoffset = 0;
+
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 500;
+	vinfo.yoffset = 0;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 1000;
+	vinfo.yoffset = 0;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	generate_data(2, buff);
+	vinfo.xoffset = 0;
+	vinfo.yoffset = 310;
+
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 500;
+	vinfo.yoffset = 310;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 1000;
+	vinfo.yoffset = 310;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+
+	generate_data(3, buff);
+	vinfo.xoffset = 0;
+	vinfo.yoffset = 620;
+
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 500;
+	vinfo.yoffset = 620;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
+	vinfo.xoffset = 1000;
+	vinfo.yoffset = 620;
+	gpt_fb_draw_copy(&vinfo, &finfo, fb_base, buff, 420, 300);
+
 
 	munmap(fb_base, screen_size);
 	close(fbfd);
