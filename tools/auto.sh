8e484e431b4eb0c9877c38618907098393d1f45c
grapebdy <grapel2013@gmail.com>
2018-03-22 22:47:42 +0800

    all: add 1080p support

--- tools/auto.sh
+++ tools/auto.sh
@@ -6,7 +6,11 @@ then
 	exit
 fi
 filename=$(basename $1 .y4m)
-
-./y4m2yuv -s $1 -o $2 -w 1280 -h 720  -f 100
-
+./y4m2yuv -s $1 -o $2 -w 1280 -h 720  -f 60
 ./grappic.exe -s $2 -w 1280 -h 720 -o $filename-640x480.yuv -x 640 -y 480 -m 0 -n 0
+
+exit
+
+./y4m2yuv -s $1 -o $2 -w 1920 -h 1080  -f 2
+./grappic.exe -s $2 -w 1920 -h 1080 -o $filename-1280x720.yuv -x 1280 -y 720 -m 0 -n 0
+./grappic.exe -s $2 -w 1920 -h 1080 -o $filename-640x480.yuv -x 640 -y 480 -m 0 -n 0
